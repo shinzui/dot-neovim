79254ec78a827749b2b2f5e4e35dd0cceabf7483
Nadeem Bitar <nadeem@gmail.com>
2017-11-04 06:40:23 -0700

    :tada: Initial neovim install

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 #
-# shinzui/nvim ellipsis package
+# shinzui/neovim ellipsis package
 
 # The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
+pkg.install() {
+  sudo mkdir /usr/local/Frameworks
+  sudo chown $(whoami):admin /usr/local/Frameworks
+  brew install neovim
+  brew install python
+  brew install python3
+  pip2 install neovim --upgrade --user
+  pip3 install neovim --upgrade
+
+}
+
+pkg.link() {
+  readonly files=(init.vim)
+
+  for file in "${files[@]}"; do
+    echo "${file}"
+    fs.link_rfile "${file}" ~/.config/neovim/init.vim
+  done
+}
 
 # pkg.push() {
 #     git.push
